d6025a24228a0286a6b474ac20e4c15b499d5609
Gølden <navejohnmichael@gmail.com>
2025-12-19 15:09:33 +0800

    chore: add color palette

--- main/tailwind.config.js
+++ main/tailwind.config.js
@@ -7,7 +7,21 @@ module.exports = {
   ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'text-dark': '#E0E0E0',
+        'background-dark': '#2B0E0E',
+        'primary-dark': '#FFC107',
+        'text-light': '#333333',
+        'background-light': '#FFFDF4',
+        'primary-light': '#E69138',
+        'secondary': '#FF5E3E',
+        'accent': '#B71C1C',
+        'light-green': '#E8F5E9',
+        'main-green': '#4CAF50',
+        'dark-green': '#388E3C',
+      },
+    },
   },
   plugins: [],
 }
